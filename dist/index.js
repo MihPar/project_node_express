982435a4eec1d15853b570432f3a24df3c76561c
MihPar <9496260@gmail.com>
2023-08-25 15:38:53 +0300

    project0825/3

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 4000;
+const port = 4000;
+const products = [
+    { title: 'tomato' },
+    { title: 'potato' },
+    { title: 'orange' }
+];
 app.get('/', function (req, res) {
     res.send('<h1>I am the samuray of back-end developer</h1>');
+});
+app.get('/products', function (req, res) {
+    res.json(products);
+});
+app.get('/products/:productTitle', function (req, res) {
+    const product = products.find(p => p.title === req.params.productTitle);
+    // res.json(product)
+    if (product) {
+        res.json(product);
+    }
+    else {
+        res.status(404).json('404 Not found');
+    }
 });
 app.listen(port, '127.0.0.1', function () {
     console.log(`Server was started at port http://loclahost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 const app = express()
 
-const port = process.env.PORT || 4000
+const port = 4000
 
-app.get('/', function(req, res) {
+const products = [
+    {title: 'tomato'},
+    {title: 'potato'},
+    {title: 'orange'}
+]
+
+app.get('/', function(req: Request, res: Response) {
     res.send('<h1>I am the samuray of back-end developer</h1>')
 })
+
+app.get('/products', function(req: Request, res: Response) {
+    res.json(products)
+})
+
+app.get('/products/:productTitle', function(req: Request, res: Response) {
+    const product = products.find(p => p.title === req.params.productTitle)
+    // res.json(product)
+    if(product) {
+        res.json(product)
+    } else {
+        res.status(404).json('404 Not found')
+    }
+})
+
 app.listen(port, '127.0.0.1', function() {
     console.log(`Server was started at port http://loclahost:${port}`)
 })
