746bfc3b4eada1c8fd4cdc047f69a4c456f607ad
MihPar <9496260@gmail.com>
2023-08-25 07:50:38 +0300

    project_node25

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const port = 4000
+
+app.get('/', function(req: any, res: any) {
+    let message = 'Goodbuy my past life'
+    res.send(message)
+})
+
+app.listen(port,  '127.0.0.1', function()  {
+    console.log(`Server was started at port http://localhost:${port}`)
+})
