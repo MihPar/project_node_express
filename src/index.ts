f2f231184df727fe1557361bb769ff36c32cf90c
MihPar <9496260@gmail.com>
2023-08-26 16:49:08 +0300

    node26

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,14 @@ const app = express()
 const port = 4000
 
 const products = [
-    {title: 'tomato'},
-    {title: 'potato'},
-    {title: 'orange'}
+    {id: 1, title: 'tomato'},
+    {id: 2, title: 'potato'},
+    {id: 3, title: 'orange'}
+]
+
+const addresses = [
+    {id: 1, value: 'Novay 12'},
+    {id: 2, value: 'Staray 13'}
 ]
 
 app.get('/', function(req: Request, res: Response) {
@@ -14,18 +19,55 @@ app.get('/', function(req: Request, res: Response) {
 })
 
 app.get('/products', function(req: Request, res: Response) {
-    res.json(products)
-})
-
-app.get('/products/:productTitle', function(req: Request, res: Response) {
-    const product = products.find(p => p.title === req.params.productTitle)
-    // res.json(product)
-    if(product) {
-        res.json(product)
+    if(req.query.title) {
+        let result = req.query.title.toString()
+        res.json(products.find(function(p) {
+            return p.title.indexOf(result) > -1    
+        }))
     } else {
-        res.status(404).json('404 Not found')
+        res.json(products)
     }
 })
+
+
+app.delete('/products/:id', function(req: Request, res: Response) {
+    for(let i = 0; i < products.length; i++) {
+        if(products[i].id === Number(req.params.id)) {
+            products.splice(i, 1)
+            res.json(204)
+            return
+        } 
+    }
+    res.json(404)
+})
+
+// app.get('/products/:productTitle', function(req: Request, res: Response) {
+//     const product = products.find(p => p.title === req.params.productTitle)
+//     // res.json(product)
+//     if(product) {
+//         res.json(product)
+//     } else {
+//         res.status(404).json('404 Not found')
+//     }
+// })
+
+// app.get('/addresses/:id', function(req: Request, res: Response) {
+//     let result
+//     for(let item of addresses) {
+//         if(item.id === Number(req.params.id)) {
+//             result = item.value
+//         }
+//     }
+//     res.json(result)
+//     // let result = addresses.find(function(address) {
+//     //     return address.id === Number(req.params.id)
+//     // })
+//     // if(result) {
+//     //     res.json(result)
+//     // } else {
+//     //     res.status(404).json('Not found')
+//     // }
+// })
 
 app.listen(port, '127.0.0.1', function() {
     console.log(`Server was started at port http://loclahost:${port}`)
